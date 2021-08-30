3f1548c4b01556ac2aa7c65665fd3c64bd82f0d1
Diaz Djuliansyah <52434835+diazdjul19@users.noreply.github.com>
2021-08-30 21:39:04 +0700

    Update download_all_siswa_received_pdf.blade.php

--- resources/views/pdf/download_all_siswa_received_pdf.blade.php
+++ resources/views/pdf/download_all_siswa_received_pdf.blade.php
@@ -6,8 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Download All Siswa Received</title>
-    
-    <link rel="stylesheet" href="C:\laragon\www\ppdb-online\public\safario\vendors\bootstrap\bootstrap.min.css">
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
 </head>
 
 <body>
