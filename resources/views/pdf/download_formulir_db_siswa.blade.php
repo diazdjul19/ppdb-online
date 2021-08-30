1d6be0ee3545a5a7c30992cd6307f4ef54cae431
Diaz Djuliansyah <52434835+diazdjul19@users.noreply.github.com>
2021-08-30 21:39:32 +0700

    Update download_formulir_db_siswa.blade.php

--- resources/views/pdf/download_formulir_db_siswa.blade.php
+++ resources/views/pdf/download_formulir_db_siswa.blade.php
@@ -7,7 +7,8 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     
-    <link rel="stylesheet" href="C:\laragon\www\ppdb-online\public\safario\vendors\bootstrap\bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
 </head>
 
 <body>
